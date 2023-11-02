8712571640fb13f6953f3dedad4aacacb567302a
tigger <tigger@cherground.com>
2023-11-02 12:31:03 +0900

    Drawing Project One

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(50, 100);
-ctx.lineTo(50, 50);
-ctx.stroke();
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+
+// ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 250, 20);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
