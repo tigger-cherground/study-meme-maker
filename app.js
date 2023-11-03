efa733c7721b287da7d2743b3068539758148572
tigger <tigger@cherground.com>
2023-11-03 17:23:02 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -5,30 +5,27 @@ canvas.height = 800;
 
 ctx.lineWitdh = 2;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",
-]
+let isPainting = false;
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
 
-function onClick(event) {
-    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
-
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
 }
 
-function onMouseMove(event) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function startPainting(event) {
+    ctx.beginPath();
+    isPainting = true;
 }
 
-canvas.addEventListener('click', onClick);
-canvas.addEventListener('mousemove', onMouseMove);
+function cancelPainting(event) {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
