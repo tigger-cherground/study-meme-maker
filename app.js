9ef0d5de2f59d738da532039648ec543df2c1d43
tigger <tigger@cherground.com>
2023-11-06 16:14:17 +0900

    Eraser

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const lineWidth = document.getElementById('line-width');
 const color = document.getElementById('color');
 const colorOptions = Array.from(document.getElementsByClassName('color-options'));
+const modeBtn = document.getElementById('mode-btn');
+const destoryBtn = document.getElementById('destory-btn');
+const eraseBtn = document.getElementById('eraser-btn');
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 800;
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -28,7 +37,7 @@ function cancelPainting(event) {
 }
 
 function onLineWidthChange(event) {
-    ctx.lineWidth = event.target.value;
+    ctx.lineWidth = event.target.value; 
 }
 function onColorChange(event) {
     ctx.strokeStyle = event.target.value;
@@ -42,12 +51,45 @@ function onColorClick(event) {
     color.value = selectColor;
 }
 
+function onModeClick(event) {
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+function onDesctoryClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling =  false;
+    modeBtn.innerText = "Fill";
+
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
-lineWidth.addEventListener("change", onLineWidthChange)
+lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(colorOption => colorOption.addEventListener('click', onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destoryBtn.addEventListener('click', onDesctoryClick);
+eraseBtn.addEventListener('click', onEraserClick);
