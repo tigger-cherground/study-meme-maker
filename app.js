d5fcacbb7ad5bb2059a19c947c7a381c88a023b3
tigger <tigger@cherground.com>
2023-11-06 15:19:51 +0900

    Line Width

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWitdh = 2;
 
 let isPainting = false;
 function onMove(event) {
@@ -17,15 +17,21 @@ function onMove(event) {
 }
 
 function startPainting(event) {
-    ctx.beginPath();
     isPainting = true;
 }
 
 function cancelPainting(event) {
     isPainting = false;
+    ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange)
